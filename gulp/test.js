8f48f86d8661585cf7ae26ac49bcb6ac61248211
cyx2288 <530656898@qq.com>
2018-06-27 22:33:25 +0800

    提交第一版

---
+++ gulp/test.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Administrator on 2018/6/22.
+ */
+var gulp = require('gulp');
+
+
+function test(done) {
+
+}
+
+
+module.exports = test;
---
+++ src/view/index/homepage/homepage.js
@@ -0,0 +1,18 @@
+new Vue({
+    el: '#app',
+    data:{
+        class1: false
+    },
+
+    methods: {
+
+        demo(){
+
+            commonJx.skipTo('http://www.taobao.com')
+
+
+        }
+
+
+    }
+});
