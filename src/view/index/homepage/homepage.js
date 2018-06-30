30bc7b43858837837df3824dbdd1a0b0d6115a3a
cyx2288 <530656898@qq.com>
2018-06-30 16:22:12 +0800

    集成

--- src/view/index/homepage/homepage.js
+++ src/view/index/homepage/homepage.js
@@ -1,14 +1,76 @@
+
+const url='http://jxtest.99payroll.cn/jx-user';//登录环境
+
+const signUrl ='/jx/action/login';//登录的url
+
+
+function json2Form(json) {
+    var str = [];
+    for(var p in json){
+        str.push(encodeURIComponent(p) + "=" + encodeURIComponent(json[p]));
+    }
+
+
+    return str.join("&");
+
+
+}
+
+
 new Vue({
+
     el: '#app',
+
     data:{
-        class1: false
+
+        num:'',
+        
+        password:""
+
     },
 
     methods: {
 
         demo(){
 
-            commonJx.skipTo('http://www.taobao.com')
+            var _this=this;
+
+            //commonJx.skipTo('http://www.taobao.com');
+
+            mui.ajax(url+signUrl,{
+
+                data:json2Form({
+
+                    mobile:_this.num,
+                    
+                    password:hex_md5(_this.password)
+
+                }),
+
+                dataType:'json',//服务器返回json格式数据
+
+                type:'post',//HTTP请求类型
+
+                timeout:10000,//超时时间设置为10秒；
+
+                headers:{
+
+                    'content-type': 'application/x-www-form-urlencoded' // post请求
+
+                },
+                success:function(data){
+                    //服务器返回响应，根据响应结果，分析是否登录成功；
+                    console.log(data)
+
+                },
+
+                error:function(xhr,type,errorThrown){
+                    //异常处理；
+                    console.log(type);
+                }
+
+            });
+
 
 
         }
